18f1d98bfb32d142d53d6d583dca11e18d45a6c9
JiukOn <jp252529@gmail.com>
2022-01-20 18:03:58 -0300

    Express

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('execute');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/status', (req, res, next) => {
+    res.status(200).send({ myGitHub: 'https://github.com/JiukOn' });
+});
+app.listen(3000, () => {
+    console.log('Server is Running');
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('execute');
+import express, {Request, Response, NextFunction} from 'express';
+
+const app = express();
+
+app.get('/status',(req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({myGitHub: 'https://github.com/JiukOn'});
+} );
+
+app.listen(3000, () => {
+    console.log('Server is Running');
+})
