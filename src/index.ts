4d0fcfc96070c25934c228c63473dc26d3a42369
JiukOn <jp252529@gmail.com>
2022-01-20 18:12:09 -0300

    Ts DEV helper

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express, {Request, Response, NextFunction} from 'express';
 const app = express();
 
 app.get('/status',(req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({myGitHub: 'https://github.com/JiukOn'});
+    res.status(200).send({myGitHub: 'https://github.com/JiukOn !'});
 } );
 
 app.listen(3000, () => {
